9302fafc64eb873b1d7329aeb5c772af3fb96550
Russell Wallace <russell.wallace@gmail.com>
2024-05-19 15:00:13 +0100

    focus

--- TypeSmart.py
+++ TypeSmart.py
@@ -7,3 +7,7 @@ root.title("TypeSmart")
 # Create a larger Text widget
 text_field = tk.Text(root, height=20, width=80, wrap=tk.WORD)
 text_field.pack(pady=10)
+text_field.focus_set()
+
+# Run the Tkinter event loop
+root.mainloop()
