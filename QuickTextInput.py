358cdff19d65f632d6d3a4e28470e06ce459b9a8
Russell Wallace <russell.wallace@gmail.com>
2024-11-09 08:00:32 +0000

    refactor

--- QuickTextInput.py
+++ QuickTextInput.py
@@ -4,28 +4,9 @@ import sqlite3
 import tkinter as tk
 from collections import defaultdict
 
-# File path
-user_profile = os.environ["USERPROFILE"]
-documents_dir = os.path.join(user_profile, "Documents")
-db_path = os.path.join(documents_dir, "QuickTextInput.db")
+import common
 
-# Connect to database
-conn = sqlite3.connect(db_path, autocommit=True)
-conn.execute("PRAGMA foreign_keys=ON")
-
-# Initialize tables
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-tables = cursor.fetchall()
-if len(tables) == 0:
-    cursor.execute(
-        """
-    CREATE TABLE words (
-        word TEXT PRIMARY KEY,
-        count INTEGER DEFAULT 0
-    ) STRICT;
-    """
-    )
+conn = common.init_db()
 
 # Read word frequencies
 word_freq = {}
