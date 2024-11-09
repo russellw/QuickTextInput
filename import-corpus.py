073bfc06b807eef6e98722e6e29edd204bf40db4
Russell Wallace <russell.wallace@gmail.com>
2024-11-09 09:41:58 +0000

    import corpus

--- import-corpus.py
+++ import-corpus.py
@@ -1,2 +1,13 @@
 import sys
 from collections import Counter, defaultdict
+
+import common
+
+corpus_file = sys.argv[0]
+conn = common.init_db()
+with open(corpus_file, "r", encoding="utf-8") as f:
+    for line in f:
+        words = (
+            line.lower().translate(str.maketrans("", "", string.punctuation)).split()
+        )
+        self.word_freq.update(words)
