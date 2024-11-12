6b1f2fb359760dba17075f3011b9adc34f23003d
Russell Wallace <russell.wallace@gmail.com>
2024-11-12 15:45:57 +0000

    db_path

--- import-corpus.py
+++ import-corpus.py
@@ -1,19 +1,49 @@
+import argparse
+import os
 import re
+import sqlite3
 import sys
 from collections import defaultdict
 
 import common
 
-file_path = sys.argv[1]
+# Set up argparse
+parser = argparse.ArgumentParser(
+    description="Open an SQLite database and import a text file."
+)
+parser.add_argument("text_file", type=str, help="Path to the text file to import")
+parser.add_argument(
+    "--db",
+    type=str,
+    default=None,
+    help="Path to the SQLite database file (default: ~/Documents/QuickTextInput.db)",
+)
+
+args = parser.parse_args()
+
+# Determine the database path
+if args.db:
+    db_path = args.db
+else:
+    # Default file path
+    user_profile = os.environ["USERPROFILE"]
+    documents_dir = os.path.join(user_profile, "Documents")
+    db_path = os.path.join(documents_dir, "QuickTextInput.db")
+
+
+# Path to the text file to import
+file_path = args.text_file
+
 
 lower_words = set()
-with open("words_alpha.txt", "r", encoding="utf-8") as f:
+with open("words.txt", "r", encoding="utf-8") as f:
     for line in f:
         line = line.rstrip()
         if line.islower():
             lower_words.add(line)
 
-conn = common.init_db()
+# Connect to database
+conn = common.init_db(db_path)
 
 
 # Dictionary to keep counts of each word, separate for lowercase and title case.
