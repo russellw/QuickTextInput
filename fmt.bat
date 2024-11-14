f05e2d0eef1866592e00d434af0cacc98cedf43c
Russell Wallace <russell.wallace@gmail.com>
2024-11-14 21:58:53 +0000

    HTML

--- fmt.bat
+++ fmt.bat
@@ -1,6 +1,11 @@
+rem Python
 black .||exit /b
 isort .||exit /b
 for %%x in (*.py) do blank-before-comment-py -w %%x||exit /b
 for %%x in (*.py) do capitalize-comments-py -w %%x||exit /b
 for %%x in (*.py) do sort-fns-py -w %%x||exit /b
+
+rem HTML
+call js-beautify --end-with-newline -t *.html -r||exit /b
+
 git diff
