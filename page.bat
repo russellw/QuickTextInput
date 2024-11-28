9e43f7a2007d57df5f4583b7c2a38f1b6d95f64a
Russell Wallace <russell.wallace@gmail.com>
2024-11-28 19:43:27 +0000

    minify

--- page.bat
+++ page.bat
@@ -1,1 +1,2 @@
-cl /EP index.html >a.html
+cl /EP index.html >a.html||exit /b
+call html-minifier a.html --collapse-whitespace --minify-css --remove-optional-tags --remove-attribute-quotes -o \t\index.html||exit /b
