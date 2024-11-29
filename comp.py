23c7e09994713ac040afb6474f6e6d92bdbe5b3d
Russell Wallace <russell.wallace@gmail.com>
2024-11-29 16:44:22 +0000

    Modified 1 file(s): comp.py

--- comp.py
+++ comp.py
@@ -1,23 +1,27 @@
 import subprocess
 import sys
+
 from PIL import Image
+
+x = 500
+y = 100
+files = [["partial-input.png", None, "partial.png"]]
+
+
+def comp(input1, input2, output):
+    # Get the size of the existing image
+    width, height = get_image_size(image_path)
+
+    # Construct and execute the ImageMagick command
+    command = ["magick", "-size", f"{width}x{height}", "xc:none", output_filename]
+    subprocess.run(command, check=True)
+    print(f"Transparent canvas created: {output_filename}")
+
 
 def get_image_size(image_path):
     with Image.open(image_path) as img:
         return img.size  # Returns (width, height)
 
-def create_transparent_canvas(image_path, output_filename='transparent.png'):
-    try:
-        # Get the size of the existing image
-        width, height = get_image_size(image_path)
-        
-        # Construct and execute the ImageMagick command
-        command = ['magick', 'convert','-size', f'{width}x{height}', 'xc:none', output_filename]
-        subprocess.run(command, check=True)
-        print(f"Transparent canvas created: {output_filename}")
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
