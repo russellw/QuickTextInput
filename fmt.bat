0f6bea54fef83305ce94dc42adbae1eee08ae46d
Russell Wallace <russell.wallace@gmail.com>
2024-11-30 11:15:25 +0000

    Modified 1 file(s): fmt.bat

--- fmt.bat
+++ fmt.bat
@@ -6,9 +6,8 @@ for %%x in (*.py) do capitalize-comments-py -w %%x||exit /b
 for %%x in (*.py) do sort-fns-py -w %%x||exit /b
 
 rem HTML
-sort-css -w index.html
 call js-beautify --end-with-newline -t index.html -r||exit /b
-sort-html-attrs -w index.html
+sort-html-attrs -w index.html||exit /b
 
 rem CSS
 call js-beautify --end-with-newline -t styles.css -r||exit /b
