2c8dd0d362af46a2e5cbdba3a8640e40b597f507
Russell Wallace <russell.wallace@gmail.com>
2024-12-10 16:47:19 +0000

    Modified 2 file(s): page.bat, styles.css

--- page.bat
+++ page.bat
@@ -1,2 +1,2 @@
-cl /EP index.html >\t\z.html||exit /b
+cl /DGAP=20px /EP index.html >\t\z.html||exit /b
 call html-minifier \t\z.html --collapse-whitespace --minify-css --remove-optional-tags --remove-attribute-quotes -o site\index.html||exit /b
