badd1bd83f031f954a781d81e23e827ea6161410
Russell Wallace <russell.wallace@gmail.com>
2024-12-15 23:13:02 +0000

    Modified 1 file(s): params.h

--- params.h
+++ params.h
@@ -1,6 +1,6 @@
 #define GAP 20px
 #define BUTTON_WIDTH 50%
-#define BUTTON_PADDING 10px 20px
+#define BUTTON_PADDING 10px
 #define ROUNDING 5px
 #define SHADOW_SIZE 1px 1px 3px
 #define FONT_SIZE 18px
