9ec09f50e1d5135c2d13774612d1d491d504fce1
Russell Wallace <russell.wallace@gmail.com>
2024-12-17 00:02:58 +0000

    Added 1 file(s): script.js; Modified 1 file(s): index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleBurger() {
+    var c = document.getElementById("burger-menu")
+    if (c.style.display === "block")
+        c.style.display = "none"
+    else
+        c.style.display = "block"
+}
