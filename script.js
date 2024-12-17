044355860cd3b761782d15c409be27411d9d9007
Russell Wallace <russell.wallace@gmail.com>
2024-12-17 23:30:01 +0000

    Modified 1 file(s): script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleBurger() {
     const s = document.getElementById("burger-symbol")
     const m = document.getElementById("burger-menu")
     if (s.textContent==="☰"){
-		s.textContent="✖"
+		s.textContent="✕"
         m.style.display = "block"
 	} else {
 		s.textContent="☰"
