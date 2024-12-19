3122b35650b41a73747ce5b29f23f27fa4fcd2d4
Russell Wallace <russell.wallace@gmail.com>
2024-12-19 23:51:48 +0000

    Modified 1 file(s): script.js

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ function toggleBurger() {
 		m.style.display = "none"
 	}
 }
+	document.getElementById('contactForm').addEventListener('submit', async (event) => {
+		event.preventDefault(); 
+		const formData = {
+			domain: 'quicktextinput.com',
+			name: '',
+			email: document.getElementById('email').value,
+			phone:'',
+			message: document.getElementById('message').value
+		};
+alert(JSON.stringify(formData))
+
+		try {
+			const response = await fetch("https://mapsyvgete.execute-api.eu-west-1.amazonaws.com/prod", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(formData)
+			});
+
+alert(response)
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+
+			const result = await response.json();
+			alert(result)
+			console.log("Success:", result);
+			// You can display a success message to the user here
+		} catch (error) {
+			console.error("Error:", error);
+			// Display an error message to the user if needed
+		}
+	});
